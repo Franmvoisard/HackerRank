992ea51af914d920ca7288fbe9b68fc01d70b367
Franco Voisard <36276842+Franmvoisard@users.noreply.github.com>
2024-11-22 21:09:09 -0500

    Insertion Sort 1 Solution

---
+++ Insertion Sort 1/Solution.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+void print_array(const vector<int>&);
+/*
+ * Complete the 'insertionSort1' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY arr
+ */
+
+void insertionSort1(int n, vector<int> arr) {
+    int lastElement = arr[n - 1];
+    
+    int i = n - 2;
+    while(i >= 0 && arr[i] > lastElement){
+        arr[i+1] = arr[i];
+        print_array(arr);
+        i--;
+    }  
+    arr[i+1] = lastElement;
+    print_array(arr);
+}
+
+void print_array(const vector<int>& arr){
+    for (int j = 0; j < arr.size(); j++) {
+        std::cout << arr[j];
+            
+        if(j < arr.size() - 1) {
+            std::cout << " ";
+        }
+        else {
+            cout << endl;
+        }
+    }
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    insertionSort1(n, arr);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
